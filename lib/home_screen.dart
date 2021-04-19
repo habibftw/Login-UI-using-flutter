bab71a66197112d03b40868919b03b6bb1c85c27
habibftw <habibulamin20@gmail.com>
2021-04-19 12:20:39 +0600

    2nd commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,47 +5,48 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+        body: Stack(
+      children: [
+        Container(
           decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("lib/images/bc.jpg"),
-              fit: BoxFit.cover,
-            ),
+              image: DecorationImage(
+                  image: AssetImage("lib/images/background.png"),
+                  fit: BoxFit.cover)),
+        ),
+        Container(
+          alignment: Alignment.topLeft,
+          margin: EdgeInsets.only(left: 50, top: 412),
+          child: Image.asset(
+            'lib/images/logo.png',
+            height: 187,
+            width: 192,
           ),
-          child: Stack(
-            children: [
-              Positioned(
-                top: 710,
-                bottom: 10,
-                left: 230,
-                right: 10,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              side: BorderSide(color: Colors.white)))),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/login');
-                  },
-                  child: Text('get started'),
+        ),
+        Container(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.only(left: 225, top: 700, bottom: 10),
+            child: SizedBox(
+              width: 144,
+              height: 60,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0),
+                    backgroundColor: MaterialStateProperty.all(
+                        Color.fromARGB(1, 47, 128, 237)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            side: BorderSide(color: Colors.white)))),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                child: Text(
+                  'Get started',
+                  style: TextStyle(fontFamily: 'Nunito', fontSize: 22),
                 ),
               ),
-              Positioned(
-                  top: 400,
-                  bottom: 300,
-                  left: 80,
-                  right: 200,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("lib/images/bruh.jpg"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ))
-            ],
-          )),
-    );
+            )),
+      ],
+    ));
   }
 }
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,122 +1,149 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-
+        body: Stack(
+      children: [
+        Container(
           decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("lib/images/bc.jpg"),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Stack(
-            //alignment: Alignment.center,
-            children: [
-              Positioned(
-                top:745,
-                bottom:5 ,
-                left: 0,
-                right: 140,
-                child: FlatButton(
+              image: DecorationImage(
+                  image: AssetImage("lib/images/background.png"),
+                  fit: BoxFit.cover)),
+        ),
+        Container(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.only(top: 730, left: 25),
+            child: Row(
+              //mainAxisAlignment: MainAxisAlignment.start,
+              //crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'New here?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+                FlatButton(
                     onPressed: () {
                       Navigator.pushNamed(context, '/registration');
                     },
-                    child: Text('Register',style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,),)),
-              ),
-              Positioned(
-                  top: 755,
-                  bottom: 10,
-                  left: 10,
-                  right: 150,
-                  child: Text('New here?',style: TextStyle(color: Colors.white),),
-              ),
-              Positioned(
-                  top: 710,
-                  bottom: 10,
-                  left: 230,
-                  right: 10,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                        shape:
-                        MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                                side: BorderSide(color: Colors.white)))),
-                    /*onPressed: () {
-                      Navigator.pop(context);
-                    },*/
-                    child: Text('Login',style: TextStyle(fontSize: 20)),
-                  ),
-              ),
-              Positioned(
-                  top: 350,
-                  bottom: 10,
-                  left: 10,
-                  right: 50,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text('Login',style: TextStyle(
-                          color: Colors.blue,
-                          fontSize: 35,
-                      fontWeight: FontWeight.bold),),
-                      Padding(padding: EdgeInsets.only(top: 20)),
-                      Text('Email',style: TextStyle(color: Colors.blue),),
-                      Container(
-                          width: 280,
-                          height: 70,
-                          padding: EdgeInsets.all(10.0),
-                          child: TextField(
-                            decoration: new InputDecoration(
-                                border: new OutlineInputBorder(
-                                  borderRadius: const BorderRadius.all(
-                                    const Radius.circular(10.0),
-                                  ),
-                                ),
-                                hintStyle: new TextStyle(color: Colors.grey),
-                                hintText: "Type in your text",
-                                fillColor: Colors.white),
-                          )
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text('Password',style: TextStyle(color: Colors.blue),),
-                      Container(
-                          width: 280,
-                          height: 70,
-                          padding: EdgeInsets.all(10.0),
-                          child: TextField(
-                            decoration: new InputDecoration(
-                                border: new OutlineInputBorder(
-                                  borderRadius: const BorderRadius.all(
-                                    const Radius.circular(10.0),
-                                  ),
-                                  borderSide: BorderSide(color: Colors.blue,width: 0)
-                                ),
-                                hintStyle: new TextStyle(color: Colors.grey),
-                                hintText: "Type in your text",
-                                fillColor: Colors.white),
-                          )
-                      ),
-
-                    ],
-                  )
-                  ),
-              Positioned(
-                  top: 590,
-                  bottom: 10,
-                  right: 0,
-                  left: 220,
-                  child: Text('Forgot Password?',style: TextStyle(color: Colors.blue),))
+                    )),
+              ],
+            )),
+        Container(
+            alignment: Alignment.topLeft,
+            margin: EdgeInsets.only(left: 225, top: 700, bottom: 10),
+            child: SizedBox(
+              width: 144,
+              height: 60,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0),
+                    backgroundColor: MaterialStateProperty.all(
+                        Color.fromARGB(1, 47, 128, 237)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                            side: BorderSide(color: Colors.white)))),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/login');
+                },
+                child: Text(
+                  'Login',
+                  style: TextStyle(fontFamily: 'Nunito', fontSize: 24),
+                ),
+              ),
+            )),
+        Container(
+          alignment: Alignment.topLeft,
+          margin: EdgeInsets.only(left: 25, top: 370),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Login',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(padding: EdgeInsets.only(top: 35)),
+              Text(
+                'Email',
+                style: TextStyle(color: Colors.blue, fontSize: 14),
+              ),
+              Padding(
+                padding: EdgeInsets.all(2.0),
+              ),
+              Container(
+                  width: 309,
+                  height: 40,
+                  child: TextField(
+                    decoration: new InputDecoration(
+                        border: new OutlineInputBorder(
+                          borderRadius: const BorderRadius.all(
+                            const Radius.circular(8.0),
+                          ),
+                        ),
+                        hintStyle: new TextStyle(color: Colors.grey),
+                        fillColor: Colors.white),
+                  )),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+              ),
+              Text(
+                'Password',
+                style: TextStyle(color: Colors.blue, fontSize: 14),
+              ),
+              Padding(
+                padding: EdgeInsets.all(2.0),
+              ),
+              Container(
+                  width: 309,
+                  height: 40,
+                  child: TextField(
+                    decoration: new InputDecoration(
+                        border: new OutlineInputBorder(
+                            borderRadius: const BorderRadius.all(
+                              const Radius.circular(8.0),
+                            ),
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 0)),
+                        hintStyle: new TextStyle(color: Colors.grey),
+                        fillColor: Colors.white),
+                  )),
             ],
-          )
           ),
-    );
+        ),
+        Container(
+          alignment: Alignment.topLeft,
+          margin: EdgeInsets.only(left: 217, top: 600),
+          child: Text(
+            'Forgot Password?',
+            style: TextStyle(color: Colors.blue, fontSize: 14),
+          ),
+        ),
+        Container(
+          alignment: Alignment.topLeft,
+          margin: EdgeInsets.only(left: 15, top: 600),
+          child: Image.asset(
+            'lib/images/Auth.png',
+            height: 65,
+            width: 187,
+          ),
+        ),
+      ],
+    ));
   }
 }
